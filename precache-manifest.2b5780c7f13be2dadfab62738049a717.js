c207f824b8cb56b2fefb3d334902f35ae041346d
Demtario134 <artik1999@wp.pl>
2019-12-14 14:28:14 +0100

    Updates

---
+++ precache-manifest.2b5780c7f13be2dadfab62738049a717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cf1935de2594205dcbd7215dbf6437",
+    "url": "/secret-santa-gift-drawer/index.html"
+  },
+  {
+    "revision": "9f2fcde4f6bfc7c81b65",
+    "url": "/secret-santa-gift-drawer/static/css/main.2cc8cf6d.chunk.css"
+  },
+  {
+    "revision": "0512f52820bf11374ea2",
+    "url": "/secret-santa-gift-drawer/static/js/2.5ac8255f.chunk.js"
+  },
+  {
+    "revision": "d9bc9aff9b81d31e8e002b5479cfe228",
+    "url": "/secret-santa-gift-drawer/static/js/2.5ac8255f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f2fcde4f6bfc7c81b65",
+    "url": "/secret-santa-gift-drawer/static/js/main.e822c253.chunk.js"
+  },
+  {
+    "revision": "b0688825ccbccde2c159",
+    "url": "/secret-santa-gift-drawer/static/js/runtime-main.97e9a30a.js"
+  },
+  {
+    "revision": "f47a4c49a27b6bed1fee82b3e430e00a",
+    "url": "/secret-santa-gift-drawer/static/media/bg.f47a4c49.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/secret-santa-gift-drawer/precache-manifest.0e97030944d6d642745e827e0e49e0a7.js"
+  "/secret-santa-gift-drawer/precache-manifest.2b5780c7f13be2dadfab62738049a717.js"
 );
 
 self.addEventListener('message', (event) => {
